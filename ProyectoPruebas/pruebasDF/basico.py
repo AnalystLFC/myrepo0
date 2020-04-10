100f40bd07f97675d13af0fe0711fbff1ad7e0f8
laura <fcacereslau@hotmail.com>
2020-04-10 13:19:17 +0200

    Leer CSV con pandas

--- ProyectoPruebas/pruebasDF/basico.py
+++ ProyectoPruebas/pruebasDF/basico.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 print("INICIO")
-
-
+datosDF = pd.read_csv(filepath_or_buffer="D:\\GITHUB_REPOS\\myrepo0\\ProyectoPruebas\\pruebasDF\\datos.csv", sep=',')
+print(datosDF.head())
 print("FIN")
+
